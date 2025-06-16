e9c0ecd2f3c912df9992c7f1439b72946e94ce01
Luca Mandrelli <luca.mandrelli@icloud.com>
2025-06-16 09:34:10 +0200

    fix: nix development config with packaging

--- flake.nix
+++ flake.nix
@@ -10,29 +10,62 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-
         python = pkgs.python3;
 
-        pythonEnv = python.withPackages (ps: with ps; [
-          pip
-          typer
-          pydantic
-          psutil
-          # Development dependencies
+        # Build the tamarin-wrapper package properly
+        tamarin-wrapper = python.pkgs.buildPythonPackage rec {
+          pname = "tamarin-wrapper";
+          version = "0.1.2";
+          format = "pyproject";
+
+          src = ./.;
+
+          nativeBuildInputs = with python.pkgs; [
+            setuptools
+            wheel
+            build
+          ];
+
+          propagatedBuildInputs = with python.pkgs; [
+            typer
+            pydantic
+            psutil
+          ];
+
+          # Don't run tests during build (they require Tamarin binaries)
+          doCheck = false;
+
+          meta = with pkgs.lib; {
+            description = "Python wrapper for Tamarin Prover with JSON recipe execution";
+            license = licenses.gpl3Plus;
+            maintainers = [ ];
+          };
+        };
+
+        # Development environment with the package and tools
+        devPythonEnv = python.withPackages (ps: with ps; [
+          # Include our package in development mode
+          tamarin-wrapper
+
+          # Development tools
           black
           isort
           autoflake
+          build
+          twine
+          pip
         ]);
+
       in
       {
+        # Main package output
+        packages.default = tamarin-wrapper;
+        packages.tamarin-wrapper = tamarin-wrapper;
+
+        # Development shell
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            pythonEnv
-            python3Packages.pip
-            python3Packages.build
-            python3Packages.setuptools
-            python3Packages.twine
-            python3Packages.wheel
+            devPythonEnv
             pre-commit
 
             # Tamarin dependencies
@@ -43,20 +76,28 @@
             zlib
             libffi
           ];
+
+          shellHook = ''
+            echo "🚀 Tamarin-wrapper development environment"
+            echo "📦 Package: tamarin-wrapper v0.1.2"
+            echo "🔧 Available commands:"
+            echo "  tamarin-wrapper --version    # Test CLI"
+            echo "  tamarin-wrapper --help       # Show help"
+            echo "  python -m build              # Build package"
+            echo "  python -m twine upload dist/* # Upload to PyPI"
+          '';
         };
 
-        packages.default = pkgs.stdenv.mkDerivation {
-          pname = "tamarin-wrapper";
-          version = "0.1.0";
-
-          src = ./.;
-
-          buildInputs = [ pythonEnv ];
-
-          installPhase = ''
-            mkdir -p $out/bin
-            cp -r . $out/
-          '';
+        # Development environment for building packages
+        devShells.packaging = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (python.withPackages (ps: with ps; [
+              build
+              twine
+              setuptools
+              wheel
+            ]))
+          ];
         };
       });
 }
