ab040cc3a9ce1dac057256db3b5c23f59577ef1b
Luca Mandrelli <luca.mandrelli@icloud.com>
2025-08-14 12:45:41 +0200

    chore: bump to v1.0.0

--- src/batch_tamarin/__init__.py
+++ src/batch_tamarin/__init__.py
@@ -1,6 +1,6 @@
 """Tamarin Python Wrapper - Run Tamarin Prover models with JSON recipes."""
 
-__version__ = "0.2.5"
+__version__ = "1.0.0"
 __author__ = "Luca Mandrelli <luca.mandrelli@icloud.com>"
 
 from .main import app
