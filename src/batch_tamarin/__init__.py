c9d93db8d32a2cf8a6d007350578ce7171cbcf49
Luca Mandrelli <luca.mandrelli@icloud.com>
2026-02-13 19:51:55 +0100

    fix(init.py): missing fields

--- src/batch_tamarin/__init__.py
+++ src/batch_tamarin/__init__.py
@@ -1,7 +1,8 @@
 """Tamarin Python Wrapper - Run Tamarin Prover models with JSON recipes."""
 
 __version__ = "1.1.2"
-__author__ = "Luca Mandrelli <luca.mandrelli@icloud.com>"
+__author__ = "Luca Mandrelli [dim green]<luca.mandrelli@icloud.com>[/dim green]"
+__contributors__ = ["Nik Stuckenbrock"]
 
 from .main import app
 
